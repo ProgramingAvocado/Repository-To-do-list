cabfb778fdcfe29349da3b7ebbb38c65c1eb0c9e
Jan <cubic.printing@seznam.cz>
2024-11-11 21:37:37 +0100

    add hour 2 of learning

--- java.js
+++ java.js
@@ -2,12 +2,24 @@ const task_container = document.querySelector(".container")
 
 
 function create_task(name) {
+
     let new_task = document.querySelector(".task-template").cloneNode(true)
+    new_task.querySelector(".not-done").onclick = e => {
+        change_status(e)
+    }
     new_task.querySelector("p").innerHTML = name
     new_task.classList.remove("task-template")
     task_container.append(new_task)
 }
 
 function addukol() {
-    create_task(task-to-add)
+    if (document.getElementById("task-to-add").value == "") {
+        alert("nic tam nemas bruh")
+    } else {
+        create_task(document.getElementById("task-to-add").value)
+    }
 }
+function change_status(e) {
+    e.target.classList.toggle("done")
+}
+
